c243b590d6592956c4aba46c81d00fdfc4fdea38
Marton Tokes <martontokes1@gmail.com>
2023-05-31 11:49:59 +0200

    timeout working, welcome unreachable

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
   const [isWelcome, setWelcome] = useState(true);
@@ -16,15 +16,15 @@ export default function App() {
 }
 
 function Welcome({ setWelcome, setEnglish }) {
+
   return (
     <>
       <h1>metro via virtual</h1>
       <h1>a virtual exhibition from Hong Kong</h1>
-      <button onClick={() => { setWelcome(false); setEnglish(true); }}>enter exhibition</button>
+      <button onClick={ engChange(setWelcome, setEnglish) }>enter exhibition</button>
       <h1>虛擬都會</h1>
       <h1>來自香港的線上展覽</h1>
-      <button onClick={() => { setWelcome(false); setEnglish(false); }}>進入展覽</button>
-
+      <button onClick={ zhChange(setWelcome, setEnglish) }>進入展覽</button>
     </>
   );
 }
@@ -43,4 +43,16 @@ function EnglishContent() {
 
 function ChineseContent() {
   return <h1>Chinese Content Rendered</h1>;
+}
+
+function engChange(sw, se) {
+
+  setTimeout(() => { setWelcome(false); setEnglish(true);}, 2000);
+
+}
+
+function zhChange(sw, se) {
+
+  setTimeout(() => { setWelcome(false); setEnglish(false); }, 2000);
+  
 }
