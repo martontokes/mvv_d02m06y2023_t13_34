7be197eeb47c6f677f8214ee124ac24c20f33645
Marton Tokes <martontokes1@gmail.com>
2023-05-31 12:10:39 +0200

    add to stash

--- src/app.js
+++ src/app.js
@@ -21,10 +21,10 @@ function Welcome({ setWelcome, setEnglish }) {
     <>
       <h1>metro via virtual</h1>
       <h1>a virtual exhibition from Hong Kong</h1>
-      <button onClick={ engChange(setWelcome, setEnglish) }>enter exhibition</button>
+      <button onClick={ setWelcome(false) setEnglish(true) }>enter exhibition</button>
       <h1>虛擬都會</h1>
       <h1>來自香港的線上展覽</h1>
-      <button onClick={ zhChange(setWelcome, setEnglish) }>進入展覽</button>
+      <button onClick={ setWelcome(false) setEnglish(true) }>進入展覽</button>
     </>
   );
 }
@@ -43,16 +43,4 @@ function EnglishContent() {
 
 function ChineseContent() {
   return <h1>Chinese Content Rendered</h1>;
-}
-
-function engChange(sw, se) {
-
-  setTimeout(() => { setWelcome(false); setEnglish(true);}, 2000);
-
-}
-
-function zhChange(sw, se) {
-
-  setTimeout(() => { setWelcome(false); setEnglish(false); }, 2000);
-  
 }
