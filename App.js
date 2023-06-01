b2280e2d48284c22e19646d24d3907477008669c
Marton Tokes <martontokes1@gmail.com>
2023-06-01 11:52:05 +0200

    step1

--- App.js
+++ App.js
@@ -2,6 +2,30 @@ import React, { useState, useEffect } from 'react';
 import {isMobile} from 'react-device-detect';
 
 export default function App() {
+
+  const [language, setLanguage] = useState=('');
+  const [page, setPage] = useState(0);
+  const [isWelcome, setWelcome] = useState(true);
+
+}
+
+function WelcomeScreen() {
+
+  return (
+    <>
+      <h1>metro via virtual</h1>
+      <h1>a virtual exhibition from Hong Kong</h1>
+      <button onClick={setEng}>enter exhibition</button>
+      <h1>虛擬都會</h1>
+      <h1>來自香港的線上展覽</h1>
+      <button onClick={setZh}>進入展覽</button>
+    </>
+  );
+  
+}
+
+function ContentScreen() {
+
 }
 
 function EnglishMenu({ handleContentChange }) {
