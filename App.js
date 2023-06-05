373efa1d92ae48d2ea8a698e2102b0d17dc35731
Marton Tokes <martontokes1@gmail.com>
2023-06-05 15:24:14 +0200

    buddha fixed

--- App.js
+++ App.js
@@ -419,7 +419,7 @@ Peter Nelson is a visual artist and academic working at the intersection of land
 
       case 'illumination':
 
-      setTimeout(() => { buddhachanger();}, 200);
+   buddhachanger();
 
       return (
 
@@ -612,7 +612,7 @@ Peter Nelson is a visual artist and academic working at the intersection of land
       case 'illumination':
 
 
-
+      buddhachanger();
 
       return (
 
@@ -621,12 +621,14 @@ Peter Nelson is a visual artist and academic working at the intersection of land
         <h4>Kat Suryna</h4>
         <a href="www.katsuryna.com" target="_blank">www.katsuryna.com</a>
         <p>《啟示》呈現了佛像在不同明暗和顔色的光線中分五個階段的轉變。佛生諸態反映了Suryna在香港生活期間所體驗到的該城的精神空間。由自然暖光所照射的佛之善相接近於香港許多佛雕的樣態，是人們心中理想。然而，Suryna亦受香港路人普遍埋頭手機屏幕的狀態所感，而描畫出佛像在屏幕冷調光線下所顯現出的邪惡之相，以指涉我們在網絡上因匿名所遇到的、或者扮演的具有攻擊性的人格。該動畫效果由Suryna 与Sandu Cojocari 共同製作。
-	Kat Suryna是一名具有哲學學術背景的具象藝術家。她受俄羅斯學院繪畫傳統訓練。她的作品曾展於愛沙尼亞、匈牙利、澳大利亞和香港。她的藝術作品專注於挖掘人體、自然有機體的圖案和建築敘事。</p>
-        <img src="/1.png"></img>
-        <img src="/2.png"></img>
-        <img src="/3.png"></img>
-        <img src="/4.png"></img>
-        <img src="/5.png"></img>
+	        Kat Suryna是一名具有哲學學術背景的具象藝術家。她受俄羅斯學院繪畫傳統訓練。她的作品曾展於愛沙尼亞、匈牙利、澳大利亞和香港。她的藝術作品專注於挖掘人體、自然有機體的圖案和建築敘事。</p>
+       <div id="buddhacontainer">
+        <img className="buddhaposition" id="buddha5" src="/5.png"></img>
+        <img className="buddhaposition" id="buddha4" src="/4.png"></img>
+        <img className="buddhaposition" id="buddha3" src="/3.png"></img>
+        <img className="buddhaposition" id="buddha2" src="/2.png"></img>
+        <img className="buddhaposition" id="buddha1" src="/1.png"></img>
+        </div>
         </>
   
       )
@@ -715,52 +717,28 @@ Peter Nelson is a visual artist and academic working at the intersection of land
 async function buddhachanger() {
 
   const buddhaimages = document.getElementsByClassName("buddhaposition");
-
   let index = 4;
   let direction = 'down';
-  let firstPeriod = 'true';
 
   setInterval(() => {
 
-      if (firstPeriod == 'true') {
-
-      firstPeriod = 'false';
-      
-        
-      } else {
-
-      if (direction == 'down') {
-
+    if (direction == 'down') {
       buddhaimages[index].classList.remove("op1");
       buddhaimages[index].classList.add("op0");
-
       index -= 1;
 
+          if (index == 1) {
+            direction = "up";
+          }
 
-      if (index == 1) {
-        direction = "up";
-
-      }
-
-      } else if (direction == 'up') {
-        
+    } else if (direction == 'up') {
       buddhaimages[index].classList.remove("op0");
       buddhaimages[index].classList.add("op1");
-
       index += 1;
 
-      if (index == 4) {
-        direction = "down";
-
-      }
-
-      }
-
-
-
+          if (index == 4) {
+            direction = "down";
+          }
     }
-
   }, 5000);
-
 }
-
