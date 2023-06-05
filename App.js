03da63bd64849e7fe88d9738a68ff3ee2be84eb8
Marton Tokes <martontokes1@gmail.com>
2023-06-05 18:03:40 +0200

    go on from here

--- App.js
+++ App.js
@@ -568,32 +568,22 @@ For more info about the idea: <span><a href="https://www.ctm-festival.de/festiva
         <a href="www.alexismailles.com" target="_blank">www.alexismailles.com</a>
         <a href="www.peteracnelson.com" target="_blank">www.peteracnelson.com</a>
         </div>
-        <p>Autosave: Redoubt is made to be a playable map for the computer game Counter-Strike: Global Offensive. It is a site-specific recreation of the WWII bunkers and tunnels of the Kowloon Peninsula built by the British during the 1930s. This piece taps into the dysfunctional military architecture of the historical site as well as the contradictions embedded in the 3D computer game technology for virtual archaeology — the more realistic the site recreation became, the less playable it became as a computer game.
-Andrew Luk is a Hong Kong artist who works across a range of media examining the intricacies of the human experience as well as the myths and histories associated with civilisation building. His work has been exhibited internationally including in Asia Society (Hong Kong), Tai Kwun Contemporary (Hong Kong), HOW Art Museum (Shanghai), and Kula Bazaar ACC (Gwangju). 
-Alexis Mailles produces hybrid installations that border the frontiers of Arte Povera and cyberpunk styles by using digital and analogue techniques. He has exhibited worldwide, including the M21 Museum (Shanghai), the 18th Street Art Center (Los Angeles), the Digital Art Center (Taipei), and Espace C.O.N.S.O.L.E (Paris). 
-Peter Nelson is a visual artist and academic working at the intersection of landscape theory and computer games. Originally trained in painting and drawing, Nelson currently produces exhibitions across a number of media, from painting and drawing, to animation, 3D printed sculpture and interactive game-based systems. He has held numerous group and solo exhibitions, including projects with HanArt TZ Gallery (Hong Kong), The National Palace Museum (Taiwan), The Sichuan Fine Art Academy Museum (Chongqing) and the K11 Art Foundation (Hong Kong). He is an Assistant Professor at Hong Kong Baptist University.</p>
-<button>Download Counter Strike: Global Offensive</button>
-<button>Download Autosave: Redoubt</button>
-<img className="arlogo" src="/arlogo.png"></img>
-<iframe
-    src="https://www.youtube.com/embed/I3Mr4dbVDy4"
-
-    allowFullScreen=""
-  />
-  <iframe
-    src="https://www.youtube.com/embed/XvERO-f8wfE"
-
-    allowFullScreen=""
-  />
-  <iframe
-    src="https://www.youtube.com/embed/S1zhsv44qlU"
-
-    allowFullScreen=""
-  />
-  <iframe
-    src="https://www.youtube.com/embed/hW2qJiNeotc"
-    allowFullScreen=""
-  />
+        <p>《自動存檔：堡壘》是為電競射擊遊戲「絕對武力：全球攻勢」的玩家所創作的地圖。該地圖還原了英軍二戰時在九龍半島修築的、以抵禦日軍侵佔的壕溝與隧道。游戲媒介的運用不但讓玩家在電競中直接體驗到該歷史遺址在軍事功能上的缺陷，而且揭示了用計算機3D游戲科技介入虛擬考古學所引發的悖論──該游戲對歷史遺址的還原度越高，它的可玩性就越打折扣。
+陸浩明是一名香港藝術家。他用不同媒介的創作來探尋人類經驗的紛繁複雜性，以及與人類文明建設相關的神話與歷史。他的作品曾展於亞洲協會（香港）、大館（香港）、昊美術館（上海）和Kula Bazaar ACC（光州）。
+亞歷克斯·麥爾斯運用數碼和模擬技術來創作混合裝置，其作品橫跨貧窮藝術與賽博朋克的前沿。他的作品曾展出於二十一世紀民生美術館（上海）、第十八街藝術中心（洛杉磯）、數位藝術中心（臺北）和Espace C.O.N.S.O.L.E（巴黎）。
+彼得·倪爾森是一名視覺藝術家和學術研究者。他的創作結合景觀理論和計算機游戲。繪畫訓練出身的倪爾森現創作的媒介包括繪畫、動畫、3D打印雕塑和交互式游戲系統。他的作品曾展於漢雅軒（香港）、國立故宮博物院（台灣）、四川美術學院美術館（重慶）和K11藝術基金會（香港）。倪爾森現任香港浸會大學助理教授。
+</p>
+<div id="armidmenu">
+<img className="arlogo" src="/arlogo.png"/>
+<button className="xp"><img src="/chcsbut.svg"></img></button>
+<button  className="xp"><img src="/charbut.svg"></img></button>
+</div>
+<div id="asytcontainer">
+  <iframe className='autosaveVid' src="https://www.youtube.com/embed/I3Mr4dbVDy4" allowFullScreen="" frameborder="no"/>
+  <iframe className='autosaveVid' src="https://www.youtube.com/embed/XvERO-f8wfE" allowFullScreen="" frameborder="no"/>
+  <iframe className='autosaveVid' src="https://www.youtube.com/embed/S1zhsv44qlU" allowFullScreen="" frameborder="no"/>
+  <iframe className='autosaveVid' src="https://www.youtube.com/embed/hW2qJiNeotc" allowFullScreen="" frameborder="no"/>
+  </div>
         </>
   
       )
@@ -604,7 +594,7 @@ Peter Nelson is a visual artist and academic working at the intersection of land
       function getVid2() {
         let getVid2 = document.getElementById("confreciframe");
 
-        getVid2.style.height = (getVid2.offsetWidth / 3.55848011083) + "px";
+        getVid2.style.height = (getVid.offsetWidth / 3.55848011083) + "px";
       }
 
       return (
